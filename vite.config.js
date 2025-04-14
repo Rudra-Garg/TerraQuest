f03d589ea56aec3638fe63ae47a558150d9dc6c1
Rudra Garg <rudra.garg22b@iiitg.ac.in>
2025-04-14 21:36:10 +0530

    feat: Add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite' // Correct import
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss() // Correctly added plugin instance
+    tailwindcss()
   ],
-  base: '/', 
+  base: process.env.NODE_ENV === 'production' ? '/geoguessr/' : '/', // Update base URL for GitHub Pages
   build: {
     outDir: 'dist',
   }
